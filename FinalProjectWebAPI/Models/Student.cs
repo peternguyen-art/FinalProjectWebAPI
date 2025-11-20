bf2b1a0b8dc185051162c338e1623ad7180f2d50
peternguyen <nguye5he@mail.uc.edu>
2025-11-20 13:24:16 -0500

    added elements for student table

--- FinalProjectWebAPI/Models/Student.cs
+++ FinalProjectWebAPI/Models/Student.cs
@@ -2,5 +2,12 @@
 {
     public class Student
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Program { get; set; }
+        public string YearInProgram { get; set; }
+
     }
 }
