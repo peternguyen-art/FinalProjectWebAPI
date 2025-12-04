92c788effb8d0d1e517aa91b6a89d26bc2370bbd
tatemount <167832762+tatemount@users.noreply.github.com>
2025-12-04 18:37:18 -0500

    Add files via upload
    created movie class

---
+++ FinalProjectWebAPI/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿namespace FinalProjectWebAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Director { get; set; }
+        public int Runtime { get; set; }
+
+        public Movie() { }
+
+        public Movie(int id, string title, string genre, string releaseDate, string director, int runtime)
+        {
+            Id = id;
+            Title = title;
+            Genre = genre;
+            ReleaseDate = releaseDate;
+            Director = director;
+            Runtime = runtime;
+        }
+    }
+}
+
